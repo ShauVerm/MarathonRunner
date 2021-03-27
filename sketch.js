236417d574193abe636a1f0afa1ff15013a25c65
ShauVerm <shaurya.pict@gmail.com>
2021-03-27 13:31:21 +0530

    Correcting Image Path

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload(){
 runnerImg = loadAnimation("images/Animation1.png","images/Animation2.png","images/Animation3.png","images/Animation4.png","images/Animation5.png","images/Animation6.png","images/Animation7.png","images/Animation8.png","images/Animation9.png","images/Animation10.png");
 cactusImg = loadImage("images/Cactus.png");
 lionImg = loadAnimation("images/Lion/Lion (1).png","images/Lion/Lion (2).png","images/Lion/Lion (3).png","images/Lion/Lion (4).png","images/Lion/Lion (5).png","images/Lion/Lion (6).png","images/Lion/Lion (7).png","images/Lion/Lion (8).png","images/Lion/Lion (9).png","images/Lion/Lion (10).png","images/Lion/Lion (11).png","images/Lion/Lion (12).png","images/Lion/Lion (13).png","images/Lion/Lion (14).png","images/Lion/Lion (15).png","images/Lion/Lion (16).png","images/Lion/Lion (17).png","images/Lion/Lion (18).png","images/Lion/Lion (19).png","images/Lion/Lion (20).png");
-rockImg = loadImage("images/rock.png");
+rockImg = loadImage("images/Rock.png");
 }
 
 function setup(){
